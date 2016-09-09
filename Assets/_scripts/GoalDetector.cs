c63c1b68e3049598d3711c40e1c8abf211461961
spillow <scott.pillow@gmail.com>
2016-09-09 00:09:49 -0700

    Added goal detector and explosion force on goal (no visual yet).

---
+++ Assets/_scripts/GoalDetector.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalDetector : MonoBehaviour {
+
+    public GameObject m_Ball;
+    public float m_BlastRadius;
+    public float m_ExplosionForce;
+
+    // Use this for initialization
+    void Start ()
+    {
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+    
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == m_Ball.tag)
+        {
+            // Ball collided, goal scored
+
+            // add explosion force to all players within the blast radius
+            foreach (var car in GameObject.FindGameObjectsWithTag("Player"))
+            {
+                var rb = car.GetComponent<Rigidbody>();
+                rb.AddExplosionForce(
+                    m_ExplosionForce,
+                    m_Ball.transform.position,
+                    m_BlastRadius);
+            }
+        }
+    }
+}
