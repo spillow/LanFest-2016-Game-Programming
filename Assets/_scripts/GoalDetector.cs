0e1167018acf486efe4cfbe5a9add2b0e138c3fd
spillow <scott.pillow@gmail.com>
2016-09-09 22:02:24 -0700

    Added explosion visual on score.

--- Assets/_scripts/GoalDetector.cs
+++ Assets/_scripts/GoalDetector.cs
@@ -6,6 +6,7 @@ public class GoalDetector : MonoBehaviour {
     public GameObject m_Ball;
     public float m_BlastRadius;
     public float m_ExplosionForce;
+    public GameObject m_Explosion;
 
     // Use this for initialization
     void Start ()
@@ -23,6 +24,14 @@ public class GoalDetector : MonoBehaviour {
         if (other.gameObject.tag == m_Ball.tag)
         {
             // Ball collided, goal scored
+
+            // Trigger visual
+            var exp = (GameObject)Instantiate(
+                m_Explosion, m_Ball.transform.position, Quaternion.identity);
+            var particle = exp.GetComponent<ParticleSystem>();
+            particle.Play();
+            m_Ball.SetActive(false);
+            Destroy(exp, 5f);
 
             // add explosion force to all players within the blast radius
             foreach (var car in GameObject.FindGameObjectsWithTag("Player"))
