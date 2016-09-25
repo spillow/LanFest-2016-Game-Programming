1020c8b4fb75284eeb71ef58ce316d6a6d0b50e6
spillow <scott.pillow@gmail.com>
2016-09-25 02:19:22 -0700

    Added visual for ball position.

---
+++ Assets/_scripts/BallProjector.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallProjector : MonoBehaviour
+{
+    public Transform m_Ball;
+    public Projector m_OuterProjector;
+    public Projector m_InnerProjector;
+
+    // Use this for initialization
+    void Start()
+    {
+        int projectorLayer = LayerMask.NameToLayer("Projector");
+        m_OuterProjector.ignoreLayers = ~(1 << projectorLayer);
+        m_InnerProjector.ignoreLayers = ~(1 << projectorLayer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = m_Ball.position;
+        m_OuterProjector.enabled = m_Ball.gameObject.activeInHierarchy;
+        m_InnerProjector.enabled = m_Ball.gameObject.activeInHierarchy;
+
+        float ballHeight = Mathf.Clamp(m_Ball.position.y, 0f, 20f);
+
+        m_InnerProjector.orthographicSize = 0.088f * ballHeight + 1.8f;
+    }
+}
