42205da991a956a7e133c5f1330bba681214cb41
spillow <scott.pillow@gmail.com>
2016-09-25 18:16:25 -0700

    Add ball sound effect and background music.

---
+++ Assets/_scripts/CollisionSound.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionSound : MonoBehaviour
+{
+    public string otherTag;
+    public AudioClip m_ClipToPlay;
+    private AudioSource m_AudioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.tag == otherTag)
+        {
+            m_AudioSource.clip = m_ClipToPlay;
+            m_AudioSource.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
