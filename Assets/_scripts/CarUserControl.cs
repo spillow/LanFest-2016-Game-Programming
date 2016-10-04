e1211681aec713ceb75e89cd81c05548b40c591f
Scott Pillow <scott.pillow@gmail.com>
2016-10-03 19:20:51 -0700

    Added first cut at rough multiplayer support.

--- Assets/_scripts/CarUserControl.cs
+++ Assets/_scripts/CarUserControl.cs
@@ -1,23 +1,50 @@
 using System;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.Networking;
 
 namespace UnityStandardAssets.Vehicles.Car
 {
     [RequireComponent(typeof (CarController))]
-    public class CarUserControl : MonoBehaviour
+    public class CarUserControl : NetworkBehaviour
     {
         private CarController m_Car; // the car controller we want to use
-        public UnityStandardAssets.Cameras.AutoCam m_Cam;
+        private UnityStandardAssets.Cameras.AutoCam m_Cam;
 
         private void Awake()
         {
+        }
+
+        public override void OnStartLocalPlayer()
+        {
+            base.OnStartLocalPlayer();
+
+            //GetComponent<MeshRenderer>().material.color = Color.red;
+
             // get the car controller
             m_Car = GetComponent<CarController>();
+
+            // get the camera rig
+            GameObject rig = GameObject.FindGameObjectWithTag("CameraRig");
+            m_Cam = rig.GetComponent<UnityStandardAssets.Cameras.AutoCam>();
+
+            // hook up the game manager
+            GameObject gameManager = GameObject.FindGameObjectWithTag("GameManager");
+            GameManager gm = gameManager.GetComponent<GameManager>();
+            gm.m_CarController = m_Car;
+            gm.enabled = true;
+
+            // hook up the camera rig
+            m_Cam.m_CarController = m_Car;
+            m_Cam.SetTarget(m_Car.transform);
+            m_Cam.enabled = true;
         }
 
         void Update()
         {
+            if (!isLocalPlayer)
+                return;
+
             bool ballcam = CrossPlatformInputManager.GetButtonDown("BallCam");
 
             if (ballcam)
@@ -28,6 +55,9 @@ namespace UnityStandardAssets.Vehicles.Car
 
         private void FixedUpdate()
         {
+            if (!isLocalPlayer)
+                return;
+
             // pass the input to the car!
             float h         = CrossPlatformInputManager.GetAxis("Horizontal");
             float v         = CrossPlatformInputManager.GetAxis("Vertical");
