94d966f85fabaad2d8db205131025f75e8cfa4aa
Scott Pillow <scott.pillow@gmail.com>
2016-10-04 20:50:26 -0700

    Added basic button layout for joining game (no functionality) and stub for extending the network lobby.

---
+++ Assets/_scripts/CustomNetworkLobby.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class CustomNetworkLobby : NetworkLobbyManager
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
