3d702cbd1f55f008b256060c20479e42be4f76da
Scott Pillow <scott.pillow@gmail.com>
2016-10-05 18:27:40 -0700

    add stub lobby hook.

---
+++ Assets/_scripts/CustomLobbyHook.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using Prototype.NetworkLobby;
+using UnityEngine.Networking;
+
+public class CustomLobbyHook : LobbyHook
+{
+    public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer)
+    {
+        Debug.Log("scene loaded for player!");
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
