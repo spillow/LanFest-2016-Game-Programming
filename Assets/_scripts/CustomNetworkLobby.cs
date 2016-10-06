4e760fd955a78af7e30dc9011e1e3c580fe91fa1
Scott Pillow <scott.pillow@gmail.com>
2016-10-05 17:47:55 -0700

    Changes before moving to prebaked network lobby.

--- Assets/_scripts/CustomNetworkLobby.cs
+++ Assets/_scripts/CustomNetworkLobby.cs
@@ -4,6 +4,21 @@ using UnityEngine.Networking;
 
 public class CustomNetworkLobby : NetworkLobbyManager
 {
+    public void OnHostGame()
+    {
+        //ConnectionConfig config;
+        var NC = StartHost();
+        this.TryToAddPlayer();
+        /*
+        var player = (GameObject)Instantiate(
+            gamePlayerPrefab,
+            Vector3.zero,
+            Quaternion.identity);
+        NetworkServer.Spawn(player);
+        */
+        //this.start
+        Debug.Log("trying to host!");
+    }
 
     // Use this for initialization
     void Start()
---
+++ Assets/_scripts/LobbyPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LobbyPlayer : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Making lobby player!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
